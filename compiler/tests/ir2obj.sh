a8d046307e822a3f0fa6d249305a51074d4a2ba0
Adam <asoliyev13@gmail.com>
2023-07-14 22:01:33 -0500

    cleaning

--- compiler/tests/ir2obj.sh
+++ compiler/tests/ir2obj.sh
@@ -4,7 +4,6 @@ SRC_FILE=$1
 SYSROOT=/opt/riscv/riscv64-unknown-elf
 GCCTOOLCHAIN=/opt/riscv/
 
-#clang-15 $SRC_FILE.ll -o $SRC_FILE --target=riscv64 -march=rv64gc --sysroot=/opt/riscv/riscv64-unknown-elf --gcc-toolchain=/opt/riscv/
 clang-15 $SRC_FILE.ll -o $SRC_FILE --target=riscv64 -march=rv64gc --sysroot=$SYSROOT --gcc-toolchain=$GCCTOOLCHAIN
 ./$SRC_FILE
 rm $SRC_FILE
