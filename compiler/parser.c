9da05d78d7ae285e4c23f455cceb616360269ad5
Adam <asoliyev13@gmail.com>
2023-08-30 00:35:05 -0500

    added 2d array

--- compiler/parser.c
+++ compiler/parser.c
@@ -361,6 +361,7 @@ struct initializer *initializer_list() {
                         cur = cur->next = calloc(1, sizeof(struct initializer));
                         cur->type = INT;
                         cur->children = initializer_list();
+                        consume(CCBR);
                 } else if (ct->kind == INTCONST) {
                         cur = cur->next = calloc(1, sizeof(struct initializer));
                         cur->type = INT;
@@ -765,7 +766,13 @@ void printExtDecl(struct ExtDecl *extDecl, int level) {
                                                 extDecl->decltor->col);
                                 if (extDecl->expr != NULL)
                                         printExpr(extDecl->expr, level + 1);
-                                else if (extDecl->init != NULL) {
+                                else if (extDecl->init != NULL && extDecl->init->children->children != NULL) {
+                                        fprintf(outfile,
+                                                "%*sInitializer\n",
+                                                (level + 1) * INDENT,
+                                                "");
+                                        printInitializer(extDecl->init->children, level + 2);
+                                } else if (extDecl->init != NULL) {
                                         fprintf(outfile,
                                                 "%*sInitializer\n",
                                                 (level + 1) * INDENT,
--- compiler/tests/parser/0001.c
+++ compiler/tests/parser/0001.c
@@ -27,7 +27,7 @@ int sum10(int a, int b, int c, int d, int e, int f, int g, int h, int i) {
 }
 
 int a[4] = {1, 2, 3, 9};
-float b[10][10];
+int b[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 0, 1, 2}};
 
 int main() {
         int a = b == 23 ? 32 : 43;
