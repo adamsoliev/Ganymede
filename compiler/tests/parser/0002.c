c356224e4069f0c9359beb282315e7551dc89207
Adam <asoliyev13@gmail.com>
2023-09-04 14:46:37 -0500

    all formats of float

--- compiler/tests/parser/0002.c
+++ compiler/tests/parser/0002.c
@@ -1,5 +1,13 @@
 int main() {
-        float a = 23.0f;
-        a = 32;
+        float a = .2f;
+        float a0 = .1250E+04f;
+        float a1 = 1.250E3f;
+        float a2 = 12.50E02f;
+        float a3 = 125.0e+1f;
+        float a4 = 1250e00f;
+        float a5 = 12500.e-01f;
+        float a6 = 125000e-2f;
+        float a7 = 1250.f;
+        a = 32.94f;
         return 0;
 }
