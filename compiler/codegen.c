fdecca307b89284bb9370257457a2a3a97cfcc6f
Adam <asoliyev13@gmail.com>
2023-09-06 16:43:48 -0500

    refactoring

--- compiler/codegen.c
+++ compiler/codegen.c
@@ -2,6 +2,27 @@
 
 // -------------- ASSUME UNLIMITED REGISTERS
 
+static void decl(struct ExtDecl *decl) { return; }
+
+static void stmt(struct stmt *statement) {
+        switch (statement->kind) {
+                case STMT_COMPOUND:
+                        for (struct block *cur = statement->body; cur != NULL; cur = cur->next) {
+                                if (cur->decl != NULL) {
+                                        decl(cur->decl);
+                                } else {
+                                        stmt(cur->stmt);
+                                }
+                        }
+                        break;
+                case RETURN: {
+                        fprintf(outfile, "  li      a5,0\n");
+                        break;
+                }
+                default: error("codegen: stmt: unimplemented");
+        }
+}
+
 // function
 static void function(struct ExtDecl *func) {
         fprintf(outfile, "  .globl %s\n", func->decltor->name);
@@ -13,7 +34,7 @@ static void function(struct ExtDecl *func) {
         fprintf(outfile, "  addi    s0,sp,16\n");
 
         // body
-        fprintf(outfile, "  li      a5,0\n");
+        stmt(func->compStmt);
 
         // epilogue
         fprintf(outfile, "  mv      a0,a5\n");
