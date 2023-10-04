ffd1500b59ce32e97476fc165e41b84cc6c5d148
Adam <asoliyev13@gmail.com>
2023-10-04 01:28:20 -0500

    parser

--- compiler/playground/main.c
+++ compiler/playground/main.c
@@ -6,23 +6,22 @@
 #include <string.h>
 
 // DATA STRUCTURES
+// clang-format off
 enum TokenType { /* KEYWORDS */
-                 INT,
-                 IF,
-                 RETURN,
-                 /* PUNCT */
-                 OPAR,
-                 CPAR,
-                 OCBR,
-                 CCBR,
-                 GT,
-                 SEMIC,
-                 ASGN,
-                 /* IDENT */
-                 IDENT,
-                 /* LITERALS */
-                 ICON,
+        /* 0 */        INT,
+        /* 1 */        IF,
+        /* 2 */        RETURN,
+        /* 3 */        OPAR,
+        /* 4 */        CPAR,
+        /* 5 */        OCBR,
+        /* 6 */        CCBR,
+        /* 7 */        GT,
+        /* 8 */        SEMIC,
+        /* 9 */        ASGN,
+       /* 10 */        IDENT,
+       /* 11 */        ICON,
 };
+// clang-format on
 
 struct Token {
         enum TokenType type;
@@ -32,6 +31,35 @@ struct Token {
         } value;
         struct Token *next;
 };
+
+struct Edecl {
+        uint64_t type;
+        char *name;
+        uint64_t value;
+        struct Edecl *next;
+};
+
+struct Stmt {
+        enum StmtType { S_IF, S_RETURN, S_COMP } type;
+        struct Expr *cond;
+        struct Stmt *then;
+
+        struct Expr *value; /* can act like 
+                                - value for 'return'
+                                - ident for 'goto' 
+                                - expr for expr-stmt
+                            */
+};
+
+struct Expr {
+        enum ExprType { E_INT, E_IDENT, E_GT } type;
+        uint64_t value;
+        char *ident;
+        struct Expr *lhs;
+        struct Expr *rhs;
+};
+
+#define TYPE_INT 0x0000000000000003  // 0000,0000,0011
 
 // UTILS
 bool iswhitespace(char c) { return c == ' ' || c == '\t' || c == '\n'; }
@@ -70,6 +98,14 @@ void addtoken(struct Token **head, struct Token **tail, struct Token *newtoken) 
                 (*tail)->next = newtoken;
                 *tail = newtoken;
         }
+}
+
+void consume(struct Token **token, enum TokenType type) {
+        if ((*token)->type != type) {
+                printf("Expected %d, but got %d\n", type, (*token)->type);
+                assert(0);
+        }
+        *token = (*token)->next;
 }
 
 /* -------------- SCANNER -------------- */
@@ -163,8 +199,131 @@ void printTokens(struct Token *head) {
 }
 
 /* -------------- PARSER -------------- */
-void parse(void) {
-        //
+void parse(struct Token *head) {
+        struct Token *current = head;
+        while (current != NULL) {
+                struct Edecl *edecl = malloc(sizeof(struct Edecl));
+
+                assert(current->type == INT);
+                edecl->type |= TYPE_INT;
+                consume(&current, INT);
+
+                assert(current->type == IDENT);
+                edecl->name = strdup(current->value.scon);
+                consume(&current, IDENT);
+
+                assert(current->type == OPAR);
+                consume(&current, OPAR);
+                assert(current->type == CPAR);
+                consume(&current, CPAR);
+
+                assert(current->type == OCBR);
+                consume(&current, OCBR);
+
+                while (current->type != CCBR) {
+                        if (current->type == INT) {
+                                struct Edecl *decl = malloc(sizeof(struct Edecl));
+
+                                assert(current->type == INT);
+                                decl->type |= TYPE_INT;
+                                consume(&current, INT);
+
+                                assert(current->type == IDENT);
+                                decl->name = strdup(current->value.scon);
+                                consume(&current, IDENT);
+
+                                assert(current->type == ASGN);
+                                consume(&current, ASGN);
+
+                                assert(current->type == ICON);
+                                decl->value = current->value.icon;
+                                consume(&current, ICON);
+
+                                assert(current->type == SEMIC);
+                                consume(&current, SEMIC);
+                        } else if (current->type == IF) {
+                                struct Stmt *ifstmt = malloc(sizeof(struct Stmt));
+                                ifstmt->type = S_IF;
+                                consume(&current, IF);
+
+                                assert(current->type == OPAR);
+                                consume(&current, OPAR);
+
+                                /* EXPR */
+                                struct Expr *cond = malloc(sizeof(struct Expr));
+
+                                /* - LHS */
+                                struct Expr *lhs = malloc(sizeof(struct Expr));
+                                assert(current->type == IDENT);
+                                lhs->ident = strdup(current->value.scon);
+                                lhs->type = E_IDENT;
+                                consume(&current, IDENT);
+
+                                /* - PARENT TYPE */
+                                assert(current->type == GT);
+                                cond->type = E_GT;
+                                consume(&current, GT);
+
+                                /* - RHS */
+                                struct Expr *rhs = malloc(sizeof(struct Expr));
+                                assert(current->type == ICON);
+                                rhs->value = current->value.icon;
+                                rhs->type = E_INT;
+                                consume(&current, ICON);
+
+                                cond->lhs = lhs;
+                                cond->rhs = rhs;
+
+                                assert(current->type == CPAR);
+                                consume(&current, CPAR);
+
+                                ifstmt->cond = cond;
+
+                                /* STMT */
+                                struct Stmt *then = malloc(sizeof(struct Stmt));
+
+                                assert(current->type == OCBR);
+                                consume(&current, OCBR);
+
+                                assert(current->type == RETURN);
+                                then->type = S_RETURN;
+                                consume(&current, RETURN);
+
+                                struct Expr *expr = malloc(sizeof(struct Expr));
+                                assert(current->type == ICON);
+                                expr->type = E_INT;
+                                expr->value = current->value.icon;
+                                consume(&current, ICON);
+
+                                assert(current->type == SEMIC);
+                                consume(&current, SEMIC);
+
+                                assert(current->type == CCBR);
+                                consume(&current, CCBR);
+
+                                ifstmt->then = then;
+                        } else {
+                                struct Stmt *stmt = malloc(sizeof(struct Stmt));
+
+                                assert(current->type == RETURN);
+                                stmt->type = S_RETURN;
+                                consume(&current, RETURN);
+
+                                struct Expr *value = malloc(sizeof(struct Expr));
+                                assert(current->type == ICON);
+                                value->type = E_INT;
+                                value->value = current->value.icon;
+                                stmt->value = value;
+                                consume(&current, ICON);
+
+                                assert(current->type == SEMIC);
+                                consume(&current, SEMIC);
+                        }
+                }
+
+                assert(current->type == CCBR);
+                consume(&current, CCBR);
+        }
 }
 
 /* -------------- CODEGEN -------------- */
@@ -176,6 +335,7 @@ int main(void) {
         char *program = "int main() { int a = 23; if (a > 10) { return 3; } return 0; }";
         struct Token *tokenlist = NULL;
         scan(program, &tokenlist);
+        parse(tokenlist);
         printTokens(tokenlist);
         return 0;
 }
