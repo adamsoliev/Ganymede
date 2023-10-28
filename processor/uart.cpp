547f693951ff9e2c94e932dfb6007e95dfda7a7e
Adam <asoliyev13@gmail.com>
2023-10-28 17:10:05 -0500

    can transmit a single letter

--- processor/uart.cpp
+++ processor/uart.cpp
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <bitset>
+#include <iostream>
 #include "Vuart.h"
 #include "verilated.h"
+
+void tick(Vuart *tb);
 
 int main(int argc, char **argv) {
     Verilated::commandArgs(argc, argv);
     Vuart *tb = new Vuart;
-    tb->i_clk = 0;
-    for (int k = 0; k < 10000000000000000000000; k++) {
-        tb->i_clk = ~tb->i_clk & 1;
-        tb->eval();
 
-        // printf("k = %2d, ", k);
-        printf("clk = %3d, ", tb->i_clk);
-        printf("led = %3d\n", tb->o_led);
+    char input = 'Z';
+    int output = 0;
+    for (int i = 0; i < 12; i++) {
+        tick(tb);
+        if (i == 0) {
+            tb->wr = 1;
+            tb->data = input;
+        } else {
+            if (i > 1 && i < 10) {
+                output = ((tb->tx & 1) ? 0x80 : 0) | (output >> 1);
+            }
+            tb->wr = 0;
+        }
     }
-
+    printf("input char:  %c\n", input);
+    printf("output char: %c\n", output);
     return 0;
+}
+
+void tick(Vuart *tb) {
+    tb->eval();
+    tb->clk = 1;
+    tb->eval();
+    tb->clk = 0;
+    tb->eval();
 }
--- processor/uart.v
+++ processor/uart.v
@@ -1,14 +1,22 @@
 `default_nettype none
 
-module uart(i_clk, o_led);
-    input wire  i_clk;
-    output wire o_led;
+module uart_tx(
+    input wire clk,
+    input wire wr,
+    input wire [7:0] data,
+    output wire tx
+);
 
-    reg [16:0] counter;
-    initial counter = 0;
-    always @(posedge i_clk) 
-        counter <= counter + 1'b1;
+    reg [8:0] l_data = 9'h1ff;
 
-    assign o_led = counter[16];
+    always @(posedge clk) begin
+        if (wr) begin
+            l_data <= { data, 1'b0 };
+        end
+        else begin
+            l_data <= {1'b1, l_data[8:1]};
+        end
+    end
+
+    assign tx = l_data[0];
 endmodule
-
