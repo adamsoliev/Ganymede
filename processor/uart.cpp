5ffbcffc1ce24fdfb129e6c3929bdfcdc64f67a9
Adam <asoliyev13@gmail.com>
2023-10-28 23:29:43 -0500

    simulate real clock

--- processor/uart.cpp
+++ processor/uart.cpp
@@ -1,39 +1,140 @@
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <bitset>
 #include <iostream>
+
 #include "Vuart.h"
 #include "verilated.h"
 
-void tick(Vuart *tb);
+#define TXIDLE 0
+#define TXDATA 1
+#define RXIDLE 0
+#define RXDATA 1
 
-int main(int argc, char **argv) {
-    Verilated::commandArgs(argc, argv);
-    Vuart *tb = new Vuart;
-
-    char input = 'Z';
-    int output = 0;
-    for (int i = 0; i < 12; i++) {
-        tick(tb);
-        if (i == 0) {
-            tb->wr = 1;
-            tb->data = input;
-        } else {
-            if (i > 1 && i < 10) {
-                output = ((tb->tx & 1) ? 0x80 : 0) | (output >> 1);
-            }
-            tb->wr = 0;
-        }
-    }
-    printf("input char:  %c\n", input);
-    printf("output char: %c\n", output);
-    return 0;
-}
+void print() { printf("here\n"); }
 
 void tick(Vuart *tb) {
-    tb->eval();
-    tb->clk = 1;
-    tb->eval();
-    tb->clk = 0;
-    tb->eval();
+        tb->eval();
+        tb->clk = 1;
+        tb->eval();
+        tb->clk = 0;
+        tb->eval();
+}
+
+class UARTSIM {
+        // And the pieces of the setup register broken out.
+        int m_baud_counts;
+
+        // UART state
+        int m_rx_baudcounter, m_rx_state, m_rx_bits, m_last_tx;
+        int m_tx_baudcounter, m_tx_state, m_tx_busy;
+        unsigned m_rx_data, m_tx_data;
+
+       public:
+        UARTSIM(void) {
+                setup(25);  // Set us up for a baud rate of CLK/25
+                m_rx_baudcounter = 0;
+                m_tx_baudcounter = 0;
+                m_rx_state = RXIDLE;
+                m_tx_state = TXIDLE;
+        }
+
+        void setup(unsigned isetup) { m_baud_counts = (isetup & 0x0ffffff); }
+
+        int operator()(const int i_tx) {
+                int o_rx = 1, nr = 0;
+
+                m_last_tx = i_tx;
+
+                if (m_rx_state == RXIDLE) {
+                        if (!i_tx) {
+                                m_rx_state = RXDATA;
+                                m_rx_baudcounter = m_baud_counts + m_baud_counts / 2 - 1;
+                                m_rx_bits = 0;
+                                m_rx_data = 0;
+                        }
+                } else if (m_rx_baudcounter <= 0) {
+                        if (m_rx_bits >= 8) {
+                                m_rx_state = RXIDLE;
+                                putchar(m_rx_data);
+                                putchar('\n');
+                                fflush(stdout);
+                        } else {
+                                m_rx_bits++;
+                                m_rx_data = ((i_tx & 1) ? 0x80 : 0) | (m_rx_data >> 1);
+                        }
+                        m_rx_baudcounter = m_baud_counts - 1;
+                } else {
+                        m_rx_baudcounter--;
+                }
+                if (m_tx_state == TXIDLE) {
+                        struct pollfd pb;
+                        pb.fd = STDIN_FILENO;
+                        pb.events = POLLIN;
+                        if (poll(&pb, 1, 0) < 0) perror("Polling error:");
+
+                        if (pb.revents & POLLIN) {
+                                char buf[1];
+
+                                nr = read(STDIN_FILENO, buf, 1);
+                                if (1 == nr) {
+                                        // << nstart_bits
+                                        m_tx_data = (-1 << 10) | ((buf[0] << 1) & 0x01fe);
+                                        m_tx_busy = (1 << (10)) - 1;
+                                        m_tx_state = TXDATA;
+                                        o_rx = 0;
+                                        m_tx_baudcounter = m_baud_counts - 1;
+                                } else if (nr < 0) {
+                                        fprintf(stderr, "UARTSIM()::read() ERR\n");
+                                }
+                        }
+                } else if (m_tx_baudcounter <= 0) {
+                        m_tx_data >>= 1;
+                        m_tx_busy >>= 1;
+                        if (!m_tx_busy)
+                                m_tx_state = TXIDLE;
+                        else
+                                m_tx_baudcounter = m_baud_counts - 1;
+                        o_rx = m_tx_data & 1;
+                } else {
+                        m_tx_baudcounter--;
+                        o_rx = m_tx_data & 1;
+                }
+
+                return o_rx;
+        }
+};
+
+int main(int argc, char **argv) {
+        Verilated::commandArgs(argc, argv);
+        Vuart *tb = new Vuart;
+        UARTSIM *uart = new UARTSIM();
+        uart->setup(868);
+
+        char input = 'm';
+        int output = 0;
+        int CLOCK_RATE_HZ = 100000000;  // 100 MHz clock
+        int hz_counter = 22;
+        bool tx_restart = false;
+        for (int i = 0; i < 12 * 868; i++) {
+                tick(tb);
+
+                if (hz_counter == 0) {
+                        hz_counter = CLOCK_RATE_HZ - 1;
+                } else {
+                        hz_counter--;
+                }
+
+                tx_restart = (hz_counter == 1);
+
+                if (tx_restart) {
+                        tb->wr = 1;
+                        tb->data = input;
+                }
+                (*uart)(tb->tx);
+        }
+        printf("input char:  %c\n", input);
+        return 0;
 }
--- processor/uart.v
+++ processor/uart.v
@@ -1,22 +1,75 @@
 `default_nettype none
 
-module uart_tx(
+module uart(
     input wire clk,
     input wire wr,
     input wire [7:0] data,
-    output wire tx
+    output wire tx,
+    output reg busy
 );
 
-    reg [8:0] l_data = 9'h1ff;
+	parameter [23:0] CLOCKS_PER_BAUD = 24'd868;
+	localparam [3:0] START	= 4'h0,
+		BIT_ZERO	= 4'h1,
+		BIT_ONE		= 4'h2,
+		BIT_TWO		= 4'h3,
+		BIT_THREE	= 4'h4,
+		BIT_FOUR	= 4'h5,
+		BIT_FIVE	= 4'h6,
+		BIT_SIX		= 4'h7,
+		BIT_SEVEN	= 4'h8,
+		LAST		= 4'h8,
+		IDLE		= 4'hf;
 
-    always @(posedge clk) begin
-        if (wr) begin
-            l_data <= { data, 1'b0 };
+	reg	[23:0]	counter;
+	reg	[3:0]	state;
+    reg [8:0] l_data;
+	reg	baud_stb;
+
+    initial	busy = 1'b0;
+	initial	state  = IDLE;
+	always @(posedge clk) begin
+        if ((wr)&&(!busy)) begin
+            { busy, state } <= { 1'b1, START };
         end
-        else begin
-            l_data <= {1'b1, l_data[8:1]};
+        else if (baud_stb) begin
+            if (state == IDLE) begin
+                { busy, state } <= { 1'b0, IDLE };
+            end
+            else if (state < LAST) begin
+                busy <= 1'b1;
+                state <= state + 1'b1;
+            end 
+            else begin
+                { busy, state } <= { 1'b1, IDLE };
+            end
         end
     end
 
+    initial	l_data = 9'h1ff;
+	always @(posedge clk) begin
+        if ((wr)&&(!busy)) begin
+            l_data <= { data, 1'b0 };
+        end
+        else if (baud_stb) begin
+            l_data <= { 1'b1, l_data[8:1] };
+        end
+    end
     assign tx = l_data[0];
+
+    initial	baud_stb = 1'b1;
+	initial	counter = 0;
+	always @(posedge clk) begin
+        if ((wr)&&(!busy)) begin
+            counter  <= CLOCKS_PER_BAUD - 1'b1;
+            baud_stb <= 1'b0;
+        end else if (!baud_stb) begin
+            baud_stb <= (counter == 24'h01);
+            counter  <= counter - 1'b1;
+        end else if (state != IDLE) begin
+            counter <= CLOCKS_PER_BAUD - 1'b1;
+            baud_stb <= 1'b0;
+        end
+    end
+
 endmodule
