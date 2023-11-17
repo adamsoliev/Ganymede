a2bc5870870b9630ed5790d7db180e0cef451bc9
Adam <asoliyev13@gmail.com>
2023-11-16 20:51:42 -0600

    addi/add

--- processor/CPU.sv
+++ processor/CPU.sv
@@ -22,8 +22,7 @@ module CPU(input    logic   clk,
     assign PCNext = PCPlus4;
 
     icache ic(
-        .address(PC[31:0]),
-        .rd(instr)
+        .address_i(PC[31:0]), .rd_o(instr)
     );
 
     ////////////////////
@@ -44,6 +43,67 @@ module CPU(input    logic   clk,
 
 
     ////////////////////
+    // EX
+    ////////////////////
+    logic [63:0] ex_SrcA, ex_SrcB;
+    registerfile rf(
+                .clk_i(clk),
+                .a1_i(id_rs1), .a2_i(id_rs2), .a3_i(id_rd),
+                .we3_i(RegWrite),
+                .wd3_i(WriteBackData),
+                .rd1_o(ex_SrcA),
+                .rd2_o(ex_SrcB)
+    );
+
+    // immSrc mux
+    logic [63:0] Imm;
+    assign Imm = id_iimm;
+
+    // aluSrc mux
+    logic [63:0] SrcB;
+    always_comb begin
+        unique case (id_opcode)
+            6'b0010011: begin // imm
+                SrcB = Imm;
+            end
+            6'b0110011: begin // rs2
+                SrcB = ex_SrcB;
+            end
+        endcase
+    end
+
+    logic [1:0] ex_alusrc;
+    logic [63:0] ex_alu_result;
+    alu alu(
+        .SrcA_i(ex_SrcA),
+        .SrcB_i(SrcB),
+        .result_o(ex_alu_result)
+    );
+
+    ////////////////////
+    // MEM
+    ////////////////////
+
+    ////////////////////
+    // WB
+    ////////////////////
+
+    // RegWrite signal
+    logic RegWrite;
+    always_comb begin
+        RegWrite = 1'b0;
+        unique case (id_opcode)
+            6'b0010011, 6'b0110011: begin 
+                RegWrite = 1'b1;
+            end
+        endcase
+    end
+
+    // RegWriteBack Date
+    logic [63:0] WriteBackData;
+    assign WriteBackData = ex_alu_result;
+
+    ////////////////////
     // DEBUG
     ////////////////////
     always_ff @(posedge clk or negedge rst) begin
@@ -58,15 +118,15 @@ module CPU(input    logic   clk,
 endmodule
 
 
-module icache(input     logic [31:0] address, 
-              output    logic [31:0] rd
+module icache(input     logic [31:0] address_i, 
+              output    logic [31:0] rd_o
 );
     logic [31:0] ICACHE[100:0];
     initial begin 
         $readmemh("./test/mem_instr", ICACHE);
     end
 
-    assign rd = ICACHE[address];
+    assign rd_o = ICACHE[address_i];
 
 endmodule
 
@@ -89,21 +149,40 @@ module dcache(input     logic        clk,
 
 endmodule
 
-module registerfile(input   logic           clk,
-                    input   logic [5:0]     a1, a2, a3,
-                    input   logic           we3,
-                    input   logic [63:0]    wd3,
-                    output  logic [63:0]    rd1,
-                    output  logic [63:0]    rd2
+module registerfile(input   logic           clk_i,
+                    input   logic [4:0]     a1_i, a2_i, a3_i,
+                    input   logic           we3_i,
+                    input   logic [63:0]    wd3_i,
+                    output  logic [63:0]    rd1_o,
+                    output  logic [63:0]    rd2_o
 );
     logic [63:0] REGS[31:0];
 
-    assign rd1 = (a1 != 0) ? REGS[a1] : 0;
-    assign rd2 = (a2 != 0) ? REGS[a2] : 0;
+    assign rd1_o = (a1_i != 0) ? REGS[a1_i] : 0;
+    assign rd2_o = (a2_i != 0) ? REGS[a2_i] : 0;
 
-    always_ff @(posedge clk) begin
-        if (we3) REGS[a3] <= wd3;
+    always_ff @(posedge clk_i) begin
+        if (we3_i) REGS[a3_i] <= wd3_i;
+
+        // DEBUG
+        $display(" x0:     %h     x1(ra): %h        x2(sp): %h        x3(gp): %h", REGS[ 0], REGS[ 1], REGS[ 2], REGS[ 3]);
+        $display(" x4(tp): %h     x5(t0): %h        x6(t1): %h        x7(t2): %h", REGS[ 4], REGS[ 5], REGS[ 6], REGS[ 7]);
+        $display(" x8(fp): %h     x9(s1): %h       x10(a0): %h       x11(a1): %h", REGS[ 8], REGS[ 9], REGS[10], REGS[11]);
+        $display("x12(a2): %h    x13(a3): %h       x14(a4): %h       x15(a5): %h", REGS[12], REGS[13], REGS[14], REGS[15]);
+        $display("x16(a6): %h    x17(a7): %h       x18(s2): %h       x19(s3): %h", REGS[16], REGS[17], REGS[18], REGS[19]);
+        $display("x20(s4): %h    x21(s5): %h       x22(s6): %h       x23(s7): %h", REGS[20], REGS[21], REGS[22], REGS[23]);
+        $display("x24(s8): %h    x25(s9): %h      x26(s10): %h      x27(s11): %h", REGS[24], REGS[25], REGS[26], REGS[27]);
+        $display("x28(t3): %h    x29(t4): %h       x30(t5): %h       x31(t6): %h", REGS[28], REGS[29], REGS[30], REGS[31]);
+        $display("\n");
     end
+
+endmodule
+
+module alu(input    logic [63:0]   SrcA_i,
+           input    logic [63:0]   SrcB_i,
+           output   logic [63:0]   result_o
+);
+    assign result_o = SrcA_i + SrcB_i;
 
 endmodule
 
