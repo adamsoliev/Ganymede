19abf81fe28ed9e8b3243a5042dcbbddd292fcd7
Adam <asoliyev13@gmail.com>
2023-11-18 23:55:58 -0600

    cleanup

--- processor/CPU.cpp
+++ processor/CPU.cpp
@@ -1,31 +1,40 @@
-#include <fstream>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "VCPU.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 
-void tick(VCPU *tb);
-
-int main(int argc, char **argv) {
-        Verilated::commandArgs(argc, argv);
-        VCPU *tb = new VCPU;
-        tb->rst == 0;
-        for (int i = 0; i < 11; i++) {
-                tick(tb);
-                tb->rst = 1;
+void tick(int tickcount, VCPU *tb, VerilatedVcdC *tfp) {
+        tb->eval();
+        if (tfp) tfp->dump(tickcount * 10 - 2);
+        tb->clk_i = 1;
+        tb->eval();
+        if (tfp) tfp->dump(tickcount * 10);
+        tb->clk_i = 0;
+        tb->eval();
+        if (tfp) {
+                tfp->dump(tickcount * 10 + 5);
+                tfp->flush();
         }
-        // double cycles = (double)tb->cycles;
-        // double instructions = (double)tb->instructions;
-        // printf("%-*s run at %.2f CPI\n", 10, test.c_str(), cycles / instructions);
-        delete tb;
-
-        return 0;
 }
 
-void tick(VCPU *tb) {
-        tb->eval();
-        tb->clk = 1;
-        tb->eval();
-        tb->clk = 0;
-        tb->eval();
+int main(int argc, char **argv) {
+        unsigned tickcount = 0;
+
+        // Call commandArgs first!
+        Verilated::commandArgs(argc, argv);
+
+        // Instantiate our design
+        VCPU *tb = new VCPU;
+
+        // Generate a trace
+        Verilated::traceEverOn(true);
+        VerilatedVcdC *tfp = new VerilatedVcdC;
+        tb->trace(tfp, 00);
+        tfp->open("CPUtrace.vcd");
+
+        for (int k = 0; k < 200; k++) {
+                tick(++tickcount, tb, tfp);
+        }
 }
