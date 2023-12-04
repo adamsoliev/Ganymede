ad87ae7038f41b8693726e01987dbc4b63759530
Adam <asoliyev13@gmail.com>
2023-12-03 19:18:55 -0600

    refactorg

--- os/kernel.c
+++ os/kernel.c
@@ -1,17 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef unsigned long uint64;
-
-#define MSTATUS_MPP_MASK (3L << 11)  // previous mode.
-#define MSTATUS_MPP_M (3L << 11)
-#define MSTATUS_MPP_S (1L << 11)
-#define MSTATUS_MPP_U (0L << 11)
-#define MSTATUS_MIE (1L << 3)  // machine-mode interrupt enable.
-
-// entry.S needs one stack
-__attribute__((aligned(16))) char stack0[4096];
-
 void main();
 
 unsigned char *uart = (unsigned char *)0x10000000;
