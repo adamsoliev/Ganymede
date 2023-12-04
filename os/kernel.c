ad8cfdededa48e2270e08ba5adac8e2a6458aa8a
Adam <asoliyev13@gmail.com>
2023-12-04 01:51:10 -0600

    console input/output

--- os/kernel.c
+++ os/kernel.c
@@ -1,20 +1,37 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define UART0 0x10000000L
+#define Reg(reg) (volatile unsigned char *)(UART0 + reg)
+#define ReadReg(reg) (*(Reg(reg)))
+#define WriteReg(reg, v) (*(Reg(reg)) = (v))
+
 void main();
 
-unsigned char *uart = (unsigned char *)0x10000000;
-void putchar(char c) {
-        *uart = c;
-        return;
+// console output
+void uartputc(int c) { WriteReg(0, c); }
+
+// console input
+int uartgetc(void) {
+        // if (ReadReg(5) & 0x01) {
+        return ReadReg(0);
+        // }
+        // return -1;
 }
 
 void print(const char *str) {
         while (*str != '\0') {
-                putchar(*str);
+                uartputc(*str);
                 str++;
         }
         return;
 }
 
-void main(void) { print("Hello world!\n"); }
+void main(void) {
+        //
+        print("Hello world!\r\n");
+        while (1) {
+                int c = uartgetc();
+                uartputc(c);
+        }
+}
