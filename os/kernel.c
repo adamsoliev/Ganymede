da92e49b443ae0d86f0b872fe603c6a19e20983a
Adam <asoliyev13@gmail.com>
2023-12-25 16:56:47 -0600

    makefile and timer interrupt

--- os/kernel.c
+++ os/kernel.c
@@ -3,5 +3,8 @@ unsigned long timer_scratch[5];
 int main() {
         // _entry jumps here
         // install kernel trap vector
+        int a = 23;
+        int b = 32;
+        int c = a + b;
         return 0;
 }
