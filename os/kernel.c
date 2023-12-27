714d87af391ba64c737fb5f0c7ae20f6fe7da6c3
Adam <asoliyev13@gmail.com>
2023-12-26 23:53:42 -0600

    process table

--- os/kernel.c
+++ os/kernel.c
@@ -1,3 +1,41 @@
+// data structures
+enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+
+struct proc {
+        enum procstate state;
+        int pid;
+        unsigned long kstack;
+        unsigned long sz;
+        struct context context;
+        char name[16];
+};
+
+struct context {
+        unsigned long ra;
+        unsigned long sp;
+
+        // callee-saved
+        unsigned long s0;
+        unsigned long s1;
+        unsigned long s2;
+        unsigned long s3;
+        unsigned long s4;
+        unsigned long s5;
+        unsigned long s6;
+        unsigned long s7;
+        unsigned long s8;
+        unsigned long s9;
+        unsigned long s10;
+        unsigned long s11;
+};
+
+//
+#define NPROC 4
+// since this OS runs on single CPU, we just have global vars for
+struct proc *cur_proc;        // currently running process
+struct context *cur_context;  // swtch() here to enter scheduler
+struct proc proc[NPROC];      // process table
+
 __attribute__((aligned(16))) char stack0[4096];
 
 unsigned long timer_scratch[5];
@@ -14,16 +52,11 @@ void scheduler(void);
 
 // _entry jumps here
 int main() {
-        // install kernel trap vector
-        w_stvec((unsigned long)kernelvec);
+        procinit();                         // init process table
+        w_stvec((unsigned long)kernelvec);  // install kernel trap vector
 
         scheduler();
         return 0;
-}
-
-void kerneltrap() {
-        // acknowledge software interrupt by clearing sip.SSIP
-        w_sip(r_sip() & (~2));
 }
 
 void scheduler(void) {
@@ -32,6 +65,25 @@ void scheduler(void) {
                 w_sstatus(r_sstatus() | (1 << 1));
                 int a = 32 + 43;
         }
+}
+
+//////////////
+// PROCESS
+//////////////
+void procinit(void) {
+        struct proc *p;
+        for (p = proc; p < &proc[NPROC]; p++) {
+                p->state = UNUSED;
+                p->kstack = 0;
+        }
+}
+
+//////////////
+// TRAP
+//////////////
+void kerneltrap() {
+        // acknowledge software interrupt by clearing sip.SSIP
+        w_sip(r_sip() & (~2));
 }
 
 // RISC-V
