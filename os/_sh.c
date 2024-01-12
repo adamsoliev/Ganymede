b98f27cdb6de446104e0caff5623ab1d8ab1b5aa
Adam <asoliyev13@gmail.com>
2024-01-12 11:38:25 -0600

    shell

--- os/_sh.c
+++ os/_sh.c
@@ -1,3 +1,17 @@
+int write(char);
+int read();
 
 // shell
-int main() { return 0; }
+int main() {
+        write('$');
+        while (1) {
+                int c = read();
+                if (c == '\r')
+                        write('\n');
+                else if (c == 0x7f)
+                        write('\b');
+                else
+                        write(c);
+        }
+        return 0;
+}
