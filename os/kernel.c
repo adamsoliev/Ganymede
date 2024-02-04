9480bbf65fde80ba9a7a71869131fe1f74cbf69f
Adam <asoliyev13@gmail.com>
2024-02-04 16:18:23 -0600

    uart read/write

--- os/kernel.c
+++ os/kernel.c
@@ -1,14 +1,31 @@
 #include "defs.h"
 
+void printf(const char *str) {
+        while (*str != '\0') {
+                uartputc(*str);
+                str++;
+        }
+        return;
+}
+
 int main() {
         uartinit();
 
-        int c = uartgetc();
-        uartputc(c);
-        uartputc('H');
-        uartputc('E');
-        uartputc('L');
-        uartputc('L');
-        uartputc('O');
+        printf("--------------------------------\r\n");
+        printf("<<      64-bit RISC-V OS      >>\r\n");
+        printf("--------------------------------\r\n");
+
+        while (1) {
+                int c = uartgetc();
+                if (c != -1) {
+                        if (c == '\r') uartputc('\n');
+                        else if (c == 0x7f) {
+                                uartputc('\b');
+                                uartputc(' ');
+                                uartputc('\b');
+                        }
+                        else uartputc(c);
+                }
+        }
         return 0;
 }
--- os/uart.c
+++ os/uart.c
@@ -36,23 +36,17 @@
 void uartinit(void) {
         // disable interrupts.
         WriteReg(IER, 0x00);
-
         // special mode to set baud rate.
         WriteReg(LCR, LCR_BAUD_LATCH);
-
         // LSB for baud rate of 38.4K.
         WriteReg(0, 0x03);
-
         // MSB for baud rate of 38.4K.
         WriteReg(1, 0x00);
-
         // leave set-baud mode,
         // and set word length to 8 bits, no parity.
         WriteReg(LCR, LCR_EIGHT_BITS);
-
         // reset and enable FIFOs.
         WriteReg(FCR, FCR_FIFO_ENABLE | FCR_FIFO_CLEAR);
-
         // enable transmit and receive interrupts.
         WriteReg(IER, IER_TX_ENABLE | IER_RX_ENABLE);
 }
