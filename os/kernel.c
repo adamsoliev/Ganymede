56ee1e9515093fe51155680d1350590b134ed013
Adam <asoliyev13@gmail.com>
2024-02-05 03:31:24 -0600

    console read/write

--- os/kernel.c
+++ os/kernel.c
@@ -3,6 +3,37 @@
 // only > 128 works, not sure why
 __attribute__ ((aligned (128))) char stack0[4096];
 
+int console_read(char *buf, int len) {
+        for (int i = 0; i < len; i++) {
+                while (1) {
+                        int c = uartgetc();
+                        if (c != -1) {
+                                buf[i] = c;
+                                if (c == '\r') { /* Enter */
+                                        buf[i] = 0;
+                                        printf("\r\n");
+                                        return i;
+                                } else if (c == 0x03) { /* Ctrl + C */ 
+                                        printf("Ctrl + C\r\n");
+                                        return 0;
+                                } else if (c == 0x7f) { /* Backspace */
+                                        c = 0;
+                                        if (i) printf("\b \b");
+                                        i = i ? i - 2 : i - 1;
+                                } else {
+                                        printf("%c", c);
+                                }
+                                break;
+                        }
+                }
+        }
+        return len - 1;
+}
+
+void console_write(char *buf, int len) {
+        for (int i = 0; i < len; i++) uartputc(buf[i]);
+}
+
 int main() {
         uartinit();
 
@@ -10,18 +41,8 @@ int main() {
         printf("<<      64-bit RISC-V OS      >>\r\n");
         printf("--------------------------------\r\n");
 
-        while (1) {
-                int c = uartgetc();
-                if (c != -1) {
-                        if (c == '\r')
-                                printf("%c", '\n');
-                        else if (c == 0x7f) {
-                                printf("\b");
-                                printf(" ");
-                                printf("\b");
-                        } else
-                                printf("%c", c);
-                }
-        }
+        char str[10];
+        int read = console_read(str, 6);
+        console_write(str, read);
         return 0;
 }
