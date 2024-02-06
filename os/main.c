4d14eae31c2c807fbf5c52ae7067375a337e74ab
Adam <asoliyev13@gmail.com>
2024-02-06 01:39:07 -0600

    small progress

--- os/main.c
+++ os/main.c
@@ -1,13 +1,12 @@
 #include "defs.h"
 
-__attribute__ ((aligned (128))) char stack0[4096];
+__attribute__((aligned(128))) char stack0[4096];
 int main(void);
 
 void start() {
         // set prev to supervisor
-        unsigned long mstatus;
-        asm volatile("csrr %0, mstatus" : "=r"(mstatus));
-        asm volatile("csrw mstatus, %0" ::"r"((mstatus & ~(3L << 11)) | (1L << 11)));
+        asm volatile("csrc mstatus, %0" ::"r"(0b11 << 11));
+        asm volatile("csrs mstatus, %0" ::"r"(0b01 << 11));
 
         // supervisor entry
         asm volatile("csrw mepc, %0" ::"r"(main));
@@ -15,6 +14,11 @@ void start() {
         // configure physical memory protection to give supervisor access to all memory
         asm volatile("csrw pmpaddr0, %0" ::"r"(0x3fffffffffffffULL));
         asm volatile("csrw pmpcfg0, %0" ::"r"(0xf));
+
+        // timer interrupt
+        // asm volatile("csrs mstatus, %0" :: "r" (0b10)); // SIE
+        // asm volatile("csrs mie, %0" :: "r" (0b101 << 5)); // MTIE & STIE
+        // asm volatile("csrs mideleg, %0" :: "r" (0b1 << 5)); // STIE
 
         // switch to supervisor
         asm volatile("mret");
