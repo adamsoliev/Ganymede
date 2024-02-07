9c7301ed1b41f5f3c0cf2bdcc3a67abde49ae476
Adam <asoliyev13@gmail.com>
2024-02-06 22:36:26 -0600

    notes and timer interrupt plan

--- os/uart.c
+++ os/uart.c
@@ -8,13 +8,13 @@
 #define Reg(reg) ((volatile unsigned char *)(UART0 + reg))
 
 void uartinit() {
-        (*(Reg(IER))) = 0x00;      // turn off interrupts
-        (*(Reg(LCR))) = (1 << 7);  // access rate setter registers
-        (*(Reg(0))) = 0x01;        // DLL
-        (*(Reg(1))) = 0x00;        // DLM
-        (*(Reg(LCR))) = 0x11;      // data size 8-bit
-        (*(Reg(FCR))) = (unsigned char) 0x111;     // clear Tx FIFO | clear Rx FIFO | Enable FIFO
-        (*(Reg(IER))) = 0x11;      // enable Tx | enable Rx
+        (*(Reg(IER))) = 0x00;                  // turn off interrupts
+        (*(Reg(LCR))) = (1 << 7);              // access rate setter registers
+        (*(Reg(0))) = 0x01;                    // DLL
+        (*(Reg(1))) = 0x00;                    // DLM
+        (*(Reg(LCR))) = 0x11;                  // data size 8-bit
+        (*(Reg(FCR))) = (unsigned char)0x111;  // clear Tx FIFO | clear Rx FIFO | Enable FIFO
+        (*(Reg(IER))) = 0x11;                  // enable Tx | enable Rx
 }
 
 void uartputc(int c) {
