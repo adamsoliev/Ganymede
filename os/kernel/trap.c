b824b301a5a729f7a8c04e7707e96148072d6a11
Adam <asoliyev13@gmail.com>
2024-02-18 23:44:03 -0600

    add usertrap to kerneltrap

--- os/kernel/trap.c
+++ os/kernel/trap.c
@@ -8,55 +8,11 @@ extern char trampoline[], uservec[], userret[];
 
 extern struct proc *cur_proc;
 
-void kerneltrap() {
-        print("kerneltrap\n");
+void trapinit() { asm volatile("csrw stvec, %0" : : "r"(kernelvec)); }
 
-        uint64 sstatus, scause, sepc;
-        asm volatile("csrr %0, sstatus" : "=r"(sstatus));
-        asm volatile("csrr %0, scause" : "=r"(scause));
-        asm volatile("csrr %0, sepc" : "=r"(sepc));
+void intr_on() { asm volatile("csrs sstatus, %0" : : "r"(1 << 1)); }  // SIE
 
-        /* INTERRUPTS */
-        if (scause == 0x8000000000000001L) {
-                printf("S-mode software interrupt\n");
-                asm volatile("csrc sip, %0" ::"r"(1 << 1));
-                if (cur_proc != 0 && cur_proc->state == RUNNING) yield();
-        } else if (scause == 0x8000000000000005L) {
-                panic("S-mode timer interrupt\n");
-        } else if (scause == 0x8000000000000009L) {
-                panic("S-mode external interrupt\n");
-        }
-        /* EXCEPTIONS */
-        else if (scause == 0 || scause == 4 || scause == 6) {
-                panic("misaligned\n");
-        } else if (scause == 1 || scause == 5 || scause == 7) {
-                panic("access fault\n");
-        } else if (scause == 2) {
-                panic("illegal instruction\n");
-        } else if (scause == 3) {
-                panic("breakpoint\n");
-        } else if (scause == 8 || scause == 9) {
-                panic("ecall\n");
-        } else if (scause == 12 || scause == 13 || scause == 15) {
-                panic("page fault\n");
-        }
-
-        asm volatile("csrw sstatus, %0" ::"r"(sstatus));
-        asm volatile("csrw sepc   , %0" ::"r"(sepc));
-}
-
-void trapinit() {
-        // install kernel trap vec
-        asm volatile("csrw stvec, %0" : : "r"(kernelvec));
-}
-
-void intr_on() {
-        asm volatile("csrs sstatus, %0" : : "r"(1 << 1));  // SIE
-}
-
-void intr_off() {
-        asm volatile("csrc sstatus, %0" : : "r"(1 << 1));  // SIE
-}
+void intr_off() { asm volatile("csrc sstatus, %0" : : "r"(1 << 1)); }  // SIE
 
 int copyin(uint64 *pagetable, char *dst, uint64 srcva, uint64 len) {
         uint64 n, va0, pa0;
@@ -76,33 +32,57 @@ int copyin(uint64 *pagetable, char *dst, uint64 srcva, uint64 len) {
         return 0;
 }
 
-void usertrap() {
-        printf("usertrap\n");
-        // install kernelvec
-        asm volatile("csrw stvec, %0" : : "r"(kernelvec));
+void kerneltrap() {
+        print("kerneltrap\n");
 
-        // save sepc
-        uint64 sepc;
-        asm volatile("csrr %0, sepc" : "=r"(sepc));
-        cur_proc->trapframe->epc = sepc;
-
-        uint64 scause;
+        uint64 sstatus, scause, sepc;
+        asm volatile("csrr %0, sstatus" : "=r"(sstatus));
         asm volatile("csrr %0, scause" : "=r"(scause));
+        asm volatile("csrr %0, sepc" : "=r"(sepc));
 
-        if (scause == 8) {  // syscall
-                printf("syscall\n");
+        if (!(sstatus & (1 << 8))) {  // U-mode
+                // save sepc
+                uint64 sepc;
+                asm volatile("csrr %0, sepc" : "=r"(sepc));
+                cur_proc->trapframe->epc = sepc;
+        }
+
+        /* INTERRUPTS */
+        if (scause == 0x8000000000000001L) {
+                printf("S-mode software interrupt\n");
+                asm volatile("csrc sip, %0" ::"r"(1 << 1));
+                if (cur_proc != 0 && cur_proc->state == RUNNING) yield();
+        } else if (scause == 0x8000000000000005L) {
+                panic("S-mode timer interrupt\n");
+        } else if (scause == 0x8000000000000009L) {
+                panic("S-mode external interrupt\n");
+        }
+        /* EXCEPTIONS */
+        else if (scause == 0 || scause == 4 || scause == 6) {
+                panic("S-mode misaligned exception\n");
+        } else if (scause == 1 || scause == 5 || scause == 7) {
+                panic("S-mode access fault exception\n");
+        } else if (scause == 2) {
+                panic("S-mode illegal instruction exception\n");
+        } else if (scause == 3) {
+                panic("S-mode breakpoint exception\n");
+        } else if (scause == 8 || scause == 9) {
+                printf("S-mode ecall exception\n");
                 cur_proc->trapframe->epc += 4;
                 intr_on();
                 char str[11];
                 copyin(cur_proc->pagetable, str, cur_proc->trapframe->a0, 11);
                 printf("%s", str);
-        } else if (scause & 1) {
-                printf("timer interrupt\n");
-                // acknowledge software interrupt
-                asm volatile("csrc sip, %0" ::"r"(1 << 1));
-                yield();
+        } else if (scause == 12 || scause == 13 || scause == 15) {
+                panic("S-mode page fault exception\n");
         }
-        usertrapret();
+
+        if (sstatus & (1 << 8)) {  // S-mode
+                asm volatile("csrw sstatus, %0" ::"r"(sstatus));
+                asm volatile("csrw sepc   , %0" ::"r"(sepc));
+        } else {
+                usertrapret();
+        }
 }
 
 void usertrapret() {
@@ -117,7 +97,7 @@ void usertrapret() {
         asm volatile("csrr %0, satp" : "=r"(ksatp));
         cur_proc->trapframe->kernel_satp = (uint64)ksatp;
         cur_proc->trapframe->kernel_sp = cur_proc->kstack + PGSIZE;
-        cur_proc->trapframe->kernel_trap = (uint64)usertrap;
+        cur_proc->trapframe->kernel_trap = (uint64)kerneltrap;
 
         // set prev mode to user and enable interrupts in that mode
         asm volatile("csrc sstatus, %0" ::"r"(1 << 8));
