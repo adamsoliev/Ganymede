e33d0e160897205be718a92092ff1677eb3c7b4a
Adam <asoliyev13@gmail.com>
2024-02-26 21:44:14 -0600

    tensor equal and more tests

--- mlframework/tensor.py
+++ mlframework/tensor.py
@@ -28,17 +28,39 @@ class Tensor:
         elif self.dim == 2:
             return [len(self.data), len(self.data[0])]
     
-    def matmul(self, other):
+    def equal(self, other):
+        if self.dim == 1:
+            if (len(self.data) != len(other.data)): return False
+            for i in range(len(self.data)):
+                if self.data[i] != other.data[i]: return False
+
+        assert self.dim == 2
         row1, col1 = self.size()
         row2, col2 = other.size()
-        if (row1 != col2 or row2 != col1): assert 0
+        if row1 != row2 or col1 != col2: return False
+        for i in range(row1):
+            for k in range(col1):
+                if self.data[i][k] != other.data[i][k]: return False
+        return True
+    
+    def matmul(self, other):
+        if self.dim == 1:
+            assert 0
 
-        result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        assert self.dim == 2
+        row1, col1 = self.size()
+        row2, col2 = other.size()
+        if (row1 != col2 or row2 != col1): 
+            print(f"matrix dimentions mismatch: ({row1, col1}) vs ({row2, col2})")
+            assert 0
+
+        result = [[0] * row1 for _ in range(col2)]
         for i in range(row1):
             for j in range(col2):
                 for k in range(col1):
                     result[i][j] += self.data[i][k] * other.data[k][j]
         return Tensor(result)
+        
 
 # ------------- ctorch API -------------
 def tensor(data):
@@ -58,28 +80,58 @@ def flatten(t1):
 
 
 # ------------- ctorch TEST -------------
-def sanitycheck():
+def test_matmul():
+    def cassert(int1, int2):
+        # pytorch
+        a = torch.tensor(in1)
+        b = torch.tensor(in2)
+        c = torch.matmul(a, b)
+        f = torch.flatten(c)
+
+        # ours
+        t1 = tensor(in1)
+        t2 = tensor(in2)
+        c1 = matmul(t1, t2)
+        f1 = flatten(c1)
+
+        for i, val in enumerate(f):
+            assert round(val.item(), 4) == round(f1.data[i], 4)
+
     in1 = [[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]]
     in2 = [[9.2, 2.3, 7.9], [4.6, 2.7, 7.3]]
+    cassert(in1, in2)
 
-    # pytorch
-    a = torch.tensor(in1)
-    b = torch.tensor(in2)
-    c = torch.matmul(a, b)
-    f = torch.flatten(c)
+def test_equal():
+    def cassert(int1, int2):
+        # --
+        a = torch.tensor(in1)
+        b = torch.tensor(in2)
+        r = a.equal(b)
+        # --
+        t1 = tensor(in1)
+        t2 = tensor(in2)
+        r1 = t1.equal(t2)
+        assert r == r1
 
-    # ours
-    t1 = tensor(in1)
-    t2 = tensor(in2)
-    c1 = matmul(t1, t2)
-    f1 = flatten(c1)
+    # dim dif, val dif
+    in1 = [[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]]
+    in2 = [[9.2, 2.3, 7.9], [4.6, 2.7, 7.3]]
+    cassert(in1, in2)
 
-    for i, val in enumerate(f):
-        assert round(val.item(), 4) == round(f1.data[i], 4)
+    # dim same, val dif
+    in1 = [[2.2, 3.1], [4.9, 5.2]]
+    in2 = [[9.2, 2.3], [4.6, 2.7]]
+    cassert(in1, in2)
+
+    # dim same, val same
+    in1 = [[2.2, 3.1], [4.9, 5.2]]
+    in2 = [[2.2, 3.1], [4.9, 5.2]]
+    cassert(in1, in2)
     
 # ------------- MAIN -------------
 def main():
-    sanitycheck()
+    test_matmul()
+    test_equal()
 
 if __name__ == '__main__':
     main()
