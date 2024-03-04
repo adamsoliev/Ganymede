e79f3d9dbf4a5eda590f3daa2be8a0a6523654e8
Adam <asoliyev13@gmail.com>
2024-03-04 11:30:28 -0600

    building models with pytorch - convolutional layers

--- mlframework/main.py
+++ mlframework/main.py
@@ -3,45 +3,48 @@
 # TODO: Manipulating tensor shapes 
 
 import torch
+import torch.functional as F
+
+class LeNet(torch.nn.Module):
+
+    def __init__(self):
+        super(LeNet, self).__init__()
+        # kernel
+            # 1 - # of input channels (black & white)
+            # 6 - # of features (output channels)
+            # 5 - kernel size (5x5)
+
+            # it outputs tensor of 6x28x28
+                # 6 - # of features
+                # 28x28 - height and width (scanning 32-pixel row by 5x5 kernel gives you 28 positions)
+        self.conv1 = torch.nn.Conv2d(1, 6, 5) 
+        self.conv2 = torch.nn.Conv2d(6, 16, 3)
+        self.fc1 = torch.nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a 2x2 window (basically in this line: 6x32x32 -1> 6x28x28 -2> 6x14x14 due to 1 - conv; 2 - maxpool)
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # Max pooling over a 2x2 window (basically in this line: 6x14x14 -> 6x12x12 -> 16x6x6)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        # 16x6x6 -> 576-element vector (reshape before feeding into fully connected layer)
+        x = x.view(-1, self.num_flat_features(x)) 
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
 def main():
-    lin = torch.nn.Linear(3,2) # 3 inputs and 2 output
-    # print(lin)
-    # print(lin.weight[0])
-    # print(lin.bias[0])
-    x = torch.rand(1,3)
-    y = lin(x) # forward pass
-
-    print(x[0].matmul(lin.weight[0]).sum().add(lin.bias[0]))
-    print(x[0].matmul(lin.weight[1]).sum().add(lin.bias[1]))
-
-    print(y)
-
-    """
-    nn.Linear(3, 2) can as well be interpreted as 3 x 2 matrix of weights and vector of length 2 of biases
-
-    weights =           # input x output matrix
-    [[w01, w02, w03]    
-     [w11, w12, w13]]
-
-    biases =            # for each output
-    [b1, b2]
-
-    when you run forward pass
-    y = lin(x)
-        under the hood, this will happen
-            o1 = x1 * w01 + x2 * w02 + x3 * w03 + b1 
-            o2 = x1 * w11 + x2 * w12 + x3 * w13 + b2 
-        y = [o1, o2]
-    """
-
-    # # below is mimicking the above with exact numbers
-    # input = torch.tensor([[0.8790, 0.9774, 0.2547]])
-    # weights = torch.tensor([[0.1656, 0.4969, -0.4972], [-0.2035, -0.2579, -0.3780]])
-    # biases = torch.tensor([0.3768, 0.3781])
-
-    # print(input[0].matmul(weights[0]).sum().add(biases[0])) 
-    # print(input[0].matmul(weights[1]).sum().add(biases[1])) 
+    nn = LeNet()
+    print(nn)
 
 if __name__ == '__main__':
     main()
