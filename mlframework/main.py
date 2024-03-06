8c4b88b2a072922fbbb84743daa1a120b455f2f0
Adam <asoliyev13@gmail.com>
2024-03-05 23:39:29 -0600

    refactor

--- mlframework/main.py
+++ mlframework/main.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 
 import torch
-from torch import nn
 import numpy as np
 
 class Tensor:
     def __init__(self, data):
         if type(data) == np.ndarray:
             self.data = data
+        elif isinstance(data, (int, float)):
+            self.data = np.array([data])
         else:
+            assert type(data) == list
             self.data = np.array(data)
     
+    def item(self):
+        assert len(self.data.shape) == 1    # 2 sides
+        assert self.data.shape[0] == 1 
+        return self.data[0]
+    
     def sum(self):
-        return self.data.sum()
+        return Tensor(self.data.sum())
     
     def matmul(self, other):
         assert type(other) == Tensor
@@ -29,9 +36,7 @@ def main():
     # ------------------ PYTORCH ------------------
     torch.set_printoptions(precision=9)
 
-    a = torch.tensor(   # 2x3
-        [[0.2606, 0.0398, 0.2312],
-         [0.4034, 0.8265, 0.7248]], requires_grad=True)
+    a = torch.tensor([[0.2606, 0.0398, 0.2312], [0.4034, 0.8265, 0.7248]], requires_grad=True)
     b = torch.tensor(   # 3x4
         [[0.2026, 0.4692, 0.6961, 0.0221],
          [0.7270, 0.7451, 0.8819, 0.2733],
@@ -48,22 +53,8 @@ def main():
 
     # f.backward() 
 
-    # ------------------ NUMPY ------------------
+    # ------------------ OURTORCH ------------------
     ta = Tensor([[0.2606, 0.0398, 0.2312], [0.4034, 0.8265, 0.7248]])
-    # na = np.array([     # 2x3
-    #     [0.2606, 0.0398, 0.2312],
-    #     [0.4034, 0.8265, 0.7248]])
-    # nb = np.array([     # 3x4
-    #     [0.2026, 0.4692, 0.6961, 0.0221],
-    #     [0.7270, 0.7451, 0.8819, 0.2733],
-    #     [0.8547, 0.2478, 0.0153, 0.8785]])
-    # nc = np.matmul(na, nb)
-    # nd = np.array([      # 2x4
-    #     [0.0315, 0.0230, 0.0625, 0.9245],
-    #     [0.6002, 0.0274, 0.2519, 0.3179]])
-    # ne = nc + nd
-    # nf = ne.sum()
-
     tb = Tensor([
         [0.2026, 0.4692, 0.6961, 0.0221],
         [0.7270, 0.7451, 0.8819, 0.2733],
@@ -75,9 +66,9 @@ def main():
     te = tc + td
     tf = te.sum()
 
-    assert round(f.item(), 5) == round(tf, 5)
+    # tf.backward() 
 
-
+    assert round(f.item(), 5) == round(tf.item(), 5)
     
 if __name__ == '__main__':
     main()
