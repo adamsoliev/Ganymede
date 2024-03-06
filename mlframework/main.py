9ce973a1009ef94b5100071b91ba292d869a0e7f
Adam <asoliyev13@gmail.com>
2024-03-06 00:25:00 -0600

    can view computational graph

--- mlframework/main.py
+++ mlframework/main.py
@@ -2,9 +2,43 @@
 
 import torch
 import numpy as np
+from graphviz import Digraph
+
+
+def trace(root):
+    # builds a set of all nodes and edges in a graph
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v.prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root):
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'}) # LR = left to right
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        # for any value in the graph, create a rectangular ('record') node for it
+        dot.node(name = uid, label = "{ %s }" % (n.label), shape='record')
+        if n.op:
+            # if this value is a result of some operation, create an op node for it
+            dot.node(name = uid + n.op, label = n.op)
+            # and connect this node to it
+            dot.edge(uid + n.op, uid)
+    for n1, n2 in edges:
+        # connect n1 to the op node of n2
+        dot.edge(str(id(n1)), str(id(n2)) + n2.op)
+    return dot
+
 
 class Tensor:
-    def __init__(self, data):
+    labelnum = 1
+
+    def __init__(self, data, children=(), op=""):
         if type(data) == np.ndarray:
             self.data = data
         elif isinstance(data, (int, float)):
@@ -12,25 +46,34 @@ class Tensor:
         else:
             assert type(data) == list
             self.data = np.array(data)
+        self.prev = set(children)
+        self.op = op
+
+        def genlabel():
+            name = f"T{Tensor.labelnum}"
+            Tensor.labelnum += 1
+            return name
+        self.label = genlabel()
     
     def item(self):
-        assert len(self.data.shape) == 1    # 2 sides
+        assert len(self.data.shape) == 1    
         assert self.data.shape[0] == 1 
         return self.data[0]
     
     def sum(self):
-        return Tensor(self.data.sum())
+        return Tensor(self.data.sum(), (self, ), "sum")
     
     def matmul(self, other):
         assert type(other) == Tensor
-        return Tensor(np.matmul(self.data, other.data))
+        return Tensor(np.matmul(self.data, other.data), (self, other), "matmul")
 
     def __add__(self, other):
         assert type(other) == Tensor
-        return Tensor(self.data + other.data)
+        return Tensor(self.data + other.data, (self, other), "+")
 
     def __repr__(self):
         return f"Tensor: \n{self.data}"
+    
 
 def main():
     # ------------------ PYTORCH ------------------
@@ -65,6 +108,9 @@ def main():
         [0.6002, 0.0274, 0.2519, 0.3179]])
     te = tc + td
     tf = te.sum()
+
+    dot = draw_dot(tf)
+    dot.view()
 
     # tf.backward() 
 
