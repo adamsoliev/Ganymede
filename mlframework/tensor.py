4e800954530825fb70db9d0d299c26e6900c3843
Adam <asoliyev13@gmail.com>
2024-04-04 23:39:38 -0500

    refactor

--- mlframework/tensor.py
+++ mlframework/tensor.py
@@ -8,6 +8,10 @@ from utils import draw_dot, gen_label
 from enum import Enum, auto
 class BinaryOps(Enum):
     ADD = auto(); SUB = auto(); MUL = auto(); DIV = auto()
+class UnaryOps(Enum):
+    NEG = auto(); LOG = auto(); EXP = auto() 
+class ReduceOps(Enum):
+    MEAN = auto()
 
 # Today's goal
 # input = np.random.randn(3, 2)
@@ -22,7 +26,7 @@ class BinaryOps(Enum):
 # print(loss.numpy())
 
 class Tensor():
-    def __init__(self, data, children=set(), op=None):
+    def __init__(self, data, children=set(), op=""):
         assert isinstance(data, (np.ndarray, int, float))
         if isinstance(data, (int, float)): 
             data = np.array([float(data)])
@@ -43,21 +47,16 @@ class Tensor():
         return (-y*self.log() - (1-y)*(1-self).log()).mean()
 
     # *** reduce ops ***
-    def mean(self):
-        return Tensor(np.mean(self.data), {self, }, "mean")
+    def mean(self): return e([self], ReduceOps.MEAN)
+        # return Tensor(np.mean(self.data), {self, }, "mean")
     
     # *** mlops (unary) ***
-    def log(self):
-        assert np.all(self.data > 0)
-        return Tensor(np.log(self.data), {self, }, "log")
-    def exp(self):
-        return Tensor(np.exp(self.data), {self, }, "exp")
-    def sigmoid(self):
-        return Tensor((1 / (1 + (-self).exp())).numpy(), {self, }, "sigmoid")
+    def log(self): return e([self], UnaryOps.LOG)
+    def exp(self): return e([self], UnaryOps.EXP)
+    def sigmoid(self): return 1 / (1 + (-self).exp())
 
     # *** op wrappers ***
-    def __neg__(self): 
-        return Tensor(-self.data, {self, }, "neg")
+    def __neg__(self): return e([self], UnaryOps.NEG)
 
     def __add__(self, x): return e([self, x], BinaryOps.ADD)
     def __sub__(self, x): return e([self, x], BinaryOps.SUB)
@@ -86,15 +85,33 @@ class Tensor():
 
 
 def e(srcs, op):
-    if isinstance(op, BinaryOps):
+    if isinstance(op, UnaryOps):
+        assert isinstance(srcs, list) and len(srcs) == 1
+        left = srcs[0]
+        if isinstance(left, (float, int)): left = Tensor(np.array([left]))
+        if (op == UnaryOps.NEG): return Tensor(-left.numpy(), {left, }, op.name)
+        elif (op == UnaryOps.LOG): 
+            assert np.all(left.numpy() > 0)
+            return Tensor(np.log(left.numpy()), {left, }, op.name)
+        elif (op == UnaryOps.EXP): return Tensor(np.exp(left.numpy()), {left, }, op.name)
+        else: assert 0
+
+    elif isinstance(op, BinaryOps):
         assert isinstance(srcs, list) and len(srcs) == 2
         left, right = srcs[0], srcs[1]
-        if isinstance(left, int): left = Tensor(left)
-        if isinstance(right, int): right = Tensor(right)
+        if isinstance(left, (float, int)): left = Tensor(np.array([left]))
+        if isinstance(right, (float, int)): right = Tensor(np.array([right]))
         if (op == BinaryOps.ADD): return Tensor(left.numpy() + right.numpy(), {left, right}, op.name)
         elif (op == BinaryOps.SUB): return Tensor(left.numpy() - right.numpy(), {left, right}, op.name)
         elif (op == BinaryOps.MUL): return Tensor(left.numpy() * right.numpy(), {left, right}, op.name)
         elif (op == BinaryOps.DIV): return Tensor(left.numpy() / right.numpy(), {left, right}, op.name)
+        else: assert 0
+
+    elif isinstance(op, ReduceOps):
+        assert isinstance(srcs, list) and len(srcs) == 1
+        left = srcs[0]
+        if isinstance(left, (float, int)): left = Tensor(np.array([left]))
+        if (op == ReduceOps.MEAN): return Tensor(np.mean(left.numpy()), {left, }, op.name)
         else: assert 0
     else:
         assert 0
