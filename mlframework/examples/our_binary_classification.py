a138008f06e43391d454e89461eb1f65d6906cd8
Adam <asoliyev13@gmail.com>
2024-04-08 00:42:51 -0500

    making progress on binary classification

--- mlframework/examples/our_binary_classification.py
+++ mlframework/examples/our_binary_classification.py
@@ -36,7 +36,7 @@ def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Ten
     # Make predictions
     model.eval()
     with torch.inference_mode():
-        y_prob_distr = model(X_to_pred_on)
+        y_prob_distr = model.forward(X_to_pred_on)
 
     y_pred = torch.round(y_prob_distr)  # binary
 
@@ -69,39 +69,108 @@ class Linear():
     def parameters(self) -> list['Tensor']:
         return [self.w, self.b]
 
-# class NN(nn.Module):
-#     def __init__(self, input_size: int, H1: int, output_size: int):
-#         super().__init__()
-#         self.linear = nn.Linear(input_size, H1)
-#         self.sigmoid1 = nn.Sigmoid()
-#         self.linear2 = nn.Linear(H1, output_size)
-#         self.sigmoid2 = nn.Sigmoid()
+class SGD:
+    def __init__(self, params: list['Tensor'], lr: float = 0.01) -> None:
+        self.learning_rate = lr
+        self.params = params
     
-#     def forward(self, x: 'Tensor') -> 'Tensor':
-#         x = self.linear(x)
-#         x = self.sigmoid1(x)
-#         x = self.linear2(x)
-#         x = self.sigmoid2(x)
-#         return x
+    def step(self) -> None:
+        for param in self.params:
+            param.data -= param.grad * self.learning_rate
 
-in1 = np.random.rand(4,5)
+class NN():
+    def __init__(self, input_size: int, H1: int, output_size: int):
+        self.linear = Linear(input_size, H1)
+        self.linear2 = Linear(H1, output_size)
+    
+    def forward(self, x: 'Tensor') -> 'Tensor':
+        x = self.linear(x)
+        x = x.sigmoid()
+        x = self.linear2(x)
+        x = x.sigmoid()
+        return x
+    
+    def parameters(self):
+        params = []
+        for _ in self.linear.parameters():
+            params.append(_)
+        for _ in self.linear2.parameters():
+            params.append(_)
+        return params
 
-pylinear = torch.nn.Linear(5, 2)
-pyt = torch.Tensor(in1)
-b = pylinear(pyt)
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
 
-t = Tensor(in1)
-linear = Linear(5, 2)
-a = linear(t)
+def main() -> None:
+    # create circles
+    n_pts = 1000
+    # (1000, 2) (1000,)
+    X, y = datasets.make_circles(n_samples=n_pts, random_state=42, noise=0.04)
 
-print(f"output shapes: {a.shape} vs {b.shape}")
-print("Outputs: ")
-print(b)
-print(a)
-# print(linear.parameters())
-# print("=============")
-# print(b)
-# for _, __ in pylinear.named_parameters():
-#     print(_, __)
+    # plt.scatter(x=X[:, 0], y=X[:, 1], c=y, cmap=plt.cm.RdYlBu)
+    # plt.show()
 
+    # create tensors
 
+    # # Split data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
+                                                        random_state=42) # make the random split reproducible
+    X_train = Tensor(X_train)
+    X_test = Tensor(X_test)
+    y_train = Tensor(y_train)
+    y_test = Tensor(y_test)
+
+    # usual stuff
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = NN(2, HL, 1)
+    # loss_fn = nn.BCELoss()
+    optimizer = SGD(model.parameters(), lr=LR)
+
+    epochs = EPOCHS
+
+    # move data to device
+    # X_train, y_train = X_train.to(device), y_train.to(device)
+    # X_test, y_test = X_test.to(device), y_test.to(device)
+
+    for epoch in range(epochs):
+        # model.train()
+
+        # TODO: need to figure out squeeze
+        y_prob_distr = model.forward(X_train).squeeze()
+        loss = y_prob_distr.binary_crossentropy(y_train)
+        acc = accuracy_fn(y_true=y_train, y_pred=torch.round(y_prob_distr))
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Testing
+        # model.eval()
+        with torch.inference_mode():
+            # 1. Forward pass
+            test_prob_distr = model.forward(X_test).squeeze()
+            # 2. Caculate loss/accuracy
+            test_loss = test_prob_distr.binary_crossentropy(y_test)
+            test_acc = accuracy_fn(y_true=y_test, y_pred=torch.round(test_prob_distr))
+
+        # Print out what's happening every 10 epochs
+        if epoch % 10 == 0:
+            print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
+    
+    assert test_acc > 90.0
+
+    # visualize 
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Train")
+    plot_decision_boundary(model, X_train, y_train)
+    plt.subplot(1, 2, 2)
+    plt.title("Test")
+    plot_decision_boundary(model, X_test, y_test)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
